6347fb765d108b6a7cf77b5bf8e910ca01c52628
koskas <koskas@yahoo.com>
2019-01-29 18:50:23 +0200

    example 2

---
+++ SearchExcel/src/main/java/searchExcel/gui/Data.java
@@ -0,0 +1,18 @@
+package searchExcel.gui;
+
+public class Data 
+{
+
+	public Data() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
